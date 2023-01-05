c26536e424ae397b94ab7f135dea93d76e9b0838
liviapz <liviapavinizeviani@gmail.com>
2023-01-05 15:14:55 -0300

    created a new file reader and storing it on a variable called 'fileReader' which is a type File Reader and attaching a new BufferedReader

---
+++ src/ReadAFile.java
@@ -0,0 +1,14 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class ReadAFile {
+
+    public static void main(String[] args) throws Throwable {
+
+        FileReader fileReader = new FileReader("Text.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        
+
+    }
+}
