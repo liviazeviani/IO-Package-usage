bd6c1ca01ac999fb5b865f3bc2605a014e1ca834
liviapz <liviapavinizeviani@gmail.com>
2023-01-05 17:22:52 -0300

    Handling exception, adding try catch block

--- src/ReadAFile.java
+++ src/ReadAFile.java
@@ -1,16 +1,30 @@
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class ReadAFile {
 
-    public static void main(String[] args) throws Throwable {
+    public static void main(String[] args)  {
 
-        FileReader fileReader = new FileReader("Text.txt");
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        FileReader fileReader;
+        BufferedReader bufferedReader = null;
+        try {
+            fileReader = new FileReader("Text.txt");
+            bufferedReader = new BufferedReader(fileReader);
+        } catch (FileNotFoundException e) {
+            System.err.println("File was not found");
+        }
+
 
         String line;
-        while ((line = bufferedReader.readLine()) != null){
+        while (true){
+            try {
+                if (!((line = bufferedReader.readLine()) != null)) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             System.out.println("Read: " + line);
 
         }
